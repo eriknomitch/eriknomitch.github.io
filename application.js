b131ccc054abcaf73ae0a67a5e4b669c980abe95
Erik Nomitch <enomitch@gmail.com>
2015-06-23 15:54:24 -0500

    adds coffee-script.js (not working)

--- application.js
+++ application.js
@@ -1,6 +1,9 @@
+
 $(document).ready(function() {
+  /*
   var template = $('#template').html();
   Mustache.parse(template);   // optional, speeds up future uses
   var rendered = Mustache.render(template, {name: "Luke"});
   $('#target').html(rendered);
+  */
 });
